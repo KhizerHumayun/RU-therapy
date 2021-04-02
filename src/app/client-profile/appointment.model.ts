0b07eb7903bab1e862d70d18d6d831e30a19d7ff
KhizerHumayun <60516459+KhizerHumayun@users.noreply.github.com>
2021-04-01 20:03:07 -0400

    Delete src/app directory

--- src/app/client-profile/appointment.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Appointment {
-    date: string;
-    time: string;
-    zoomLink: string;
-    id?: string;
-}
--- src/app/client-profile/post.model.ts
+++
@@ -1,9 +0,0 @@
-import { Time } from '../counselor-profile/time.model';
-
-export interface Post {
-    netid: string;
-    fullName: string;
-    zoomLink: string;
-    schedule: Time[];
-    id?: string;
-}
